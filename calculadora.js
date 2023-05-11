5a1393cecb3e26cfc28cd5cd695cb67930d46252
unknown <2224290077@ACADEMICO.IESB.BR>
2023-05-11 20:30:24 -0300

    Calculadora Devops pronta com as 4 operações

---
+++ calculadora.js
@@ -0,0 +1,23 @@
+function somar(a,b){
+    return a+b
+}
+console.log ('A soma de 2+9: ', somar(2,9))
+exports.somar = somar
+
+function multiplicar(a,b){
+    return a*b
+}
+console.log ('A multiplicação de 2x9: ', multiplicar(2,9))
+exports.multiplicar = multiplicar
+
+function subtrair(a,b){
+    return a-b
+}
+console.log ('A subtração de 2-9: ', subtrair(2,9))
+exports.subtrair = subtrair
+
+function dividir(a,b){
+    return a/b
+}
+console.log ('A divisão de 2/9: ', dividir(2,9))
+exports.dividir = dividir
